ea397a967076fe784bac7dbaabef9786d672a529
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-16 23:01:38 +0200

    update create ticket action

--- app/actions/ticket.actions.ts
+++ app/actions/ticket.actions.ts
@@ -1,14 +1,53 @@
 "use server";
+import prisma from "../db/prisma";
+import { revalidatePath } from "next/cache";
+import { logEvent } from "../utils/sentry";
 
 export async function createTicket(
   prevState: { success: boolean; message: string },
   formData: FormData
 ): Promise<{ success: boolean; message: string }> {
-  const subject = formData.get("subject") as string;
-  const description = formData.get("description") as string;
-  const priority = formData.get("priority") as string;
+  try {
+    const subject = formData.get("subject") as string;
+    const description = formData.get("description") as string;
+    const priority = formData.get("priority") as string;
 
-  console.log(subject, description, priority)
+    if (!subject || !description || !priority) {
+      logEvent(
+        "Validation Error: Missing ticket fields",
+        "ticket",
+        { subject, description, priority },
+        "warning"
+      );
+      return { success: false, message: "All fields are required" };
+    }
 
-  return { success: true, message: "Ticket create successfully" };
+    //create ticket
+    const ticket = await prisma.ticket.create({
+      data: { subject, description, priority },
+    });
+
+    logEvent(
+      `Ticket was created successfully : ${ticket.id}`,
+      "ticket",
+      { ticketId: ticket.id },
+      "info"
+    );
+
+    revalidatePath("/tickets");
+
+    return { success: true, message: "Ticket create successfully" };
+  } catch (error) {
+    logEvent(
+      "An error occurred while creating ticket",
+      "ticket",
+      { formData: Object.fromEntries(formData.entries()) },
+      "error",
+      error
+    );
+  }
+  return {
+    success: false,
+    message: "An error occurred while creating the ticket",
+  };
 }
