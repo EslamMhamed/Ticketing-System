0b2a99d065d4b98e0326807b0385da160335f891
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-16 11:56:22 +0200

    create ticket action

---
+++ app/actions/ticket.actions.ts
@@ -0,0 +1,14 @@
+"use server";
+
+export async function createTicket(
+  prevState: { success: boolean; message: string },
+  formData: FormData
+): Promise<{ success: boolean; message: string }> {
+  const subject = formData.get("subject") as string;
+  const description = formData.get("description") as string;
+  const priority = formData.get("priority") as string;
+
+  console.log(subject, description, priority)
+
+  return { success: true, message: "Ticket create successfully" };
+}
