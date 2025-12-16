3ca6106a1390e634b54651b02ff4883bd2e75066
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-16 23:02:07 +0200

    update NewTicketsPage

--- app/tickets/new/page.tsx
+++ app/tickets/new/page.tsx
@@ -1,24 +1,44 @@
+"use client"
+import { createTicket } from "@/app/actions/ticket.actions";
+import { useRouter } from "next/navigation";
+import { useActionState, useEffect } from "react";
+
 function NewTicketsPage() {
+
+    const [state, formAction ] = useActionState(createTicket, {success: false, message: ""})
+    const router = useRouter()
+
+    useEffect(()=>{
+      if(state.success){
+        router.push("/tickets")
+      }
+    }, [state.success, router])
+
   return (
     <div className="min-h-screen bg-blue-50 flex items-center justify-center px-4">
       <div className="w-full max-w-md bg-white shadow-md rounded-lg p-8 border border-gray-200">
         <h1 className="text-3xl font-bold mb-6 text-center text-blue-600">
           Submit a Support Ticket
         </h1>
-        <form className="space-y-4 text-gray-700">
+        {state.message && !state.success && (
+            <p className="text-red-500 mb-4 text-center">
+                {state.message}
+            </p>
+        ) }
+        <form action={formAction} className="space-y-4 text-gray-700">
           <input
             type="text"
             className="w-full border border-gray-200 p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400  "
             name="subject"
             placeholder="Subject"
-            required
+            
           />
           <textarea
             className="w-full border border-gray-200 p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-700"
             name="description"
             placeholder="Describe your issue"
             rows={4}
-            required
+            
           />
           <select
             className="w-full border border-gray-200 p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 text-gray-700"
