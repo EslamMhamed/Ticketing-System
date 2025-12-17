b7a215130ecc3b037b846f820dbcf1b7659876be
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-17 21:58:37 +0200

    create RegisterPage

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { registerUser } from "@/app/actions/auth.actions";
+import { useRouter } from "next/navigation";
+import { useActionState, useEffect } from "react";
+import { toast } from "sonner";
+
+function RegisterPage() {
+  const [state, formAction] = useActionState(registerUser, {
+    success: false,
+    message: "",
+  });
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (state.success) {
+      toast.success("Registration successful!");
+      router.push("/tickets");
+      router.refresh();
+    }else if(state.message){
+        toast.error(state.message)
+    }
+  }, [state, router]);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-50 px-4">
+      <div className="w-full max-w-md bg-white shadow-md rounded-lg p-8 border border-gray-200">
+        <h1 className="text-3xl font-bold mb-6 text-center text-blue-600">
+          Register
+        </h1>
+        <form action={formAction} className="space-y-4 text-gray-700">
+          <input
+            type="text"
+            className="w-full border border-gray-200 p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            name="name"
+            placeholder="Your Name"
+            required
+          />
+          <input
+            type="email"
+            className="w-full border border-gray-200 p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            name="email"
+            placeholder="Your Email"
+            autoComplete="email"
+            required
+          />
+          <input
+            type="password"
+            className="w-full border border-gray-200 p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            name="password"
+            placeholder="Password"
+            autoComplete="new-password"
+            required
+          />
+          <button
+            className="w-full bg-blue-600 text-white p-3 hover:bg-blue-700 transition disabled:opacity-50"
+            type="submit"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default RegisterPage;
