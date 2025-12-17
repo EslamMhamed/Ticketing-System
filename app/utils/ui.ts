1e9df8eebfb3570a2405b4775ef3fe5466764515
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-17 13:05:30 +0200

    create priority class

---
+++ app/utils/ui.ts
@@ -0,0 +1,10 @@
+export const getPriorityClass = (priority: string)=>{
+  switch(priority){
+    case "high":
+      return "text-red-600 font-bold"
+    case "medium": 
+      return "text-yellow-600 font-bold"
+    case "low": 
+      return "text-green-600 font-bold"
+  }
+}
